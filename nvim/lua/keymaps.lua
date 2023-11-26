5078593c63d6cca468813d609ec8e336f02b6796
Charlotte Thomas <charlotte.thomas.1@etudiant.univ-rennes.fr>
2023-11-26 19:08:34 +0100

    keymaps in keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,1 +1,33 @@
+-- [[ Basic Keymaps ]]
 
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({"n", "v"}, "<Space>", "<Nop>", {silent = true})
+
+-- Remap for dealing with word wrap
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", {expr = true, silent = true})
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", {expr = true, silent = true})
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, {desc = "Go to previous diagnostic message"})
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, {desc = "Go to next diagnostic message"})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {desc = "Open floating diagnostic message"})
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {desc = "Open diagnostics list"})
+vim.keymap.set("n", "<C-Left>", "<Cmd>BufferPrevious<CR>")
+vim.keymap.set("n", "<C-Right>", "<Cmd>BufferNext<CR>")
+vim.keymap.set("n", "<C-s>", "<Cmd>w<CR>")
+vim.keymap.set("n", "<C-a>", "<Cmd>NvimTreeToggle<CR>")
+vim.keymap.set("n", "<C-b>", "<Cmd>NvimTreeFocus<CR>")
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", {clear = true})
+vim.api.nvim_create_autocmd(
+    "TextYankPost",
+    {
+        callback = function()
+            vim.highlight.on_yank()
+        end,
+        group = highlight_group,
+        pattern = "*"
+    }
+)
