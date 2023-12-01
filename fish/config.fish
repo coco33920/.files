9267a815afd955e07e96e94b6f0fd63c18430693
Charlotte Thomas <charlotte.thomas.1@etudiant.univ-rennes.fr>
2023-12-01 19:30:45 +0100

    modify fish config

--- fish/config.fish
+++ fish/config.fish
@@ -11,7 +11,6 @@ fish_ssh_agent
 ssh-add /home/charlotte/.ssh/id_github
 ssh-add /home/charlotte/.ssh/id_gitlab_istic
 ssh-add /home/charlotte/.ssh/id_rennes
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/charlotte/.ghcup/bin # ghcup-env
 set TYPST /home/charlotte/.local/lib/typst
 set TYPST_FONT_PATH /home/charlotte/.local/share/fonts
 starship init fish | source
