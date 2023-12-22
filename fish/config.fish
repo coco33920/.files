35c3733f091ee0bf540a0cde7d93f56683b4d604
Charlotte Thomas <charlotte.thomas.1@etudiant.univ-rennes.fr>
2023-12-22 20:31:37 +0100

    add opam config

--- fish/config.fish
+++ fish/config.fish
@@ -1,22 +1,18 @@
-
 function fish_greeting
     echo ~~ Hello Sweetie ~~
     echo Telling you what time it is would be revealing ~~ spoilers ~~
 end
 
 
-#eval (opam env)
 setsid wal -i ~/Images/bg.png 2> /dev/null
 fish_ssh_agent
 ssh-add /home/charlotte/.ssh/id_github
 ssh-add /home/charlotte/.ssh/id_github_rennes
-#ssh-add /home/charlotte/.ssh/id_rennes
 set TYPST /home/charlotte/.config/typst_templates
 set PATH $PATH:/home/charlotte/.cargo/bin:/home/charlotte/.local/bin:/usr/sbin
 set TYPST_FONT_PATH /usr/share/fonts
 set EDITOR nvim
 set VISUAL nvim
-#set HOSTALIASES /home/charlotte/host.aliases
 starship init fish | source
 alias ls="eza -l --icons=always --group-directories-first --hyperlink --git --git-repos-no-status"
 
@@ -26,3 +22,6 @@ if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# opam configuration
+source /home/charlotte/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
